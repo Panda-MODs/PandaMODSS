1fc1916f9713d24aff543278d7006016c6b05c2b
Panda-MODs <98682745+Panda-MODs@users.noreply.github.com>
2022-02-06 22:45:13 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById('FiNo_v4').addEventListener('click', ()=>{
+    setTimeout(function(){
+        window.open('https://www.mediafire.com/file/3vm90ab8q3pae03/NoteFive_V4.0_by_PandaMODS.apk/file', '_blank');
+    }, 500);
+});
